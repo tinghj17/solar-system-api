1e2b1c88b17c8e03353d08c33d8ed067871a2e6d
Sophia Wagner <sophiewagnerla@gmail.com>
2022-04-26 11:38:16 -0700

    Wave 2 Changes

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Planet:
     def __init__(self, id, name, description, radius_mi):
@@ -28,4 +28,53 @@ def give_planets():
             }
         )
     return jsonify(planets_response)
+
+def validate_planet(planet_id):
+    # Want to return a valid planet object
+    # If invalid integer ID, we want to print out some error message with 404
+    # If noninteger ID, we want to print out some error message with 400
+    try:
+        planet_id = int(planet_id)
+    except ValueError:
+        abort(make_response({'msg': f"ID:{planet_id} invalid. Input an integer."}, 400))
+    
+    for planet in planets:
+        if planet_id == planet.id:
+            return planet
+
+    abort(make_response({'msg': f'Planet {planet_id} not found'}, 404))
+
+
+
+@planets_bp.route("/<planet_id>", methods=["GET"])
+def get_one_planet(planet_id):
+    planet = validate_planet(planet_id)
+    planet = {
+                "id": planet.id,
+                "name": planet.name,
+                "description": planet.description,
+                "radius_mi": planet.radius_mi
+            }
+    return planet, 200
+
+
+
+# @planets_bp.route("/<planet_id>", methods=["GET"])
+# def get_one_planet(planet_id):
+#     try:
+#         planet_id = int(planet_id)
+#     except ValueError:
+#         return {'msg': f"ID:{planet_id} invalid. Input an integer."}, 400
+#     chosen_planet = None
+#     for planet in planets:
+#         if planet_id == planet.id:
+#             chosen_planet = {
+#                 "id": planet.id,
+#                 "name": planet.name,
+#                 "description": planet.description,
+#                 "radius_mi": planet.radius_mi
+#             }
+#     if chosen_planet is None:
+#         return {'msg': f'Planet {planet_id} not found'}, 404
+#     return chosen_planet, 200
 
