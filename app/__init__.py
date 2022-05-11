31821139815bbdf8c67de04b45792c983a74bdb3
tinghj17 <tinghj17@gmail.com>
2022-05-11 11:09:36 -0700

    adds .env

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
+
 
 # creating a new database object 
 db = SQLAlchemy()
 migrate = Migrate()
+load_dotenv()
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_systems_development'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ. get("SQLALCHEMY_DATABASE_URI")
 
     # connecting db and migrate to our flask 
     db.init_app(app)
